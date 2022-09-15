0306b90fc42685312815311f9a94c2064a09527d
Ankit Lakhiwal <lakhiwalankit0002@gmail.com>
2022-09-15 23:21:48 +0530

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <View className="bg-red-400">
+      <Text className="text-5xl text-white font-bold">Hello World</Text>
+    </View>
+  )
+}
+
+export default HomeScreen
